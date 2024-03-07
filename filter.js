63eec351c468531fe28331c366d69ed6ae2091ee
Janko <janko.huic@gmail.com>
2024-03-07 05:19:45 +0100

    looks like filtering based on categories works

--- filter.js
+++ filter.js
@@ -5,27 +5,55 @@ var callback = function(mutationsList, observer) {
             if(iframe) {
                 var dropdown = iframe.contentDocument.getElementById("repertoireSelector");
                 if(dropdown) {
+                    var items = dropdown.children;
+                    var itemslength = items.length;
+
                     chrome.runtime.sendMessage({method: "getBlacklist"}, function(response) {
                         console.log("sending storage request");
                         if (response.blacklist){
                             console.log("blacklist received");
                             console.log(response.blacklist);     
                     
-                            var items = dropdown.children;
-                            var itemslength = items.length;
+                            
                             var blacklistlength = response.blacklist.length;
-                                for (var i = itemslength - 1; i >= 0; i--) {
-                                    for (let j = 0; j < blacklistlength; j++) {
-                                        if (items[i].innerText.toLowerCase().includes(response.blacklist[j].toLowerCase())) {
-                                            console.log("removing " + items[i].innerText);
-                                            dropdown.remove(i);
-                                            break;
-                                        }
+
+                            for (var i = itemslength - 1; i >= 0; i--) {
+                                for (let j = 0; j < blacklistlength; j++) {
+                                    if (items[i].innerText.toLowerCase().includes(response.blacklist[j].toLowerCase())) {
+                                        console.log("removing " + items[i].innerText);
+                                        dropdown.remove(i);
+                                        break;
                                     }
                                 }
+                            }
                         }
                     });
+                    
+                    chrome.runtime.sendMessage({method: "getCategories"}, function(response) {
+                        console.log(response.categories)
+                        var newDropdown = document.createElement("select");
+                        newDropdown.id = "newDropdown";
+                        var categories = Object.keys(response.categories);
 
+                        for (var k = 0; k < categories.length; k++){
+                            var optgroup = document.createElement("optgroup");
+                            optgroup.label = categories[k];
+                            whitelist = response.categories[categories[k]];
+                            //newDropdown.appendChild(optgroup);
+                            dropdown.appendChild(optgroup);
+                            for (var i = items.length - 1; i >= 0; i--) {
+                                for (let j = 0; j < response.categories[categories[k]].length; j++) {
+                                    console.log(items[i].innerText)
+                                    console.log(response.categories[categories[k]][j])
+                                    if (items[i].innerText.toLowerCase().includes(response.categories[categories[k]][j].toLowerCase())) {
+                                        console.log("adding " + items[i].innerText + " to " + categories[k]);
+                                        optgroup.appendChild(items[i]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    });
                     observer.disconnect();
                 }
             }
