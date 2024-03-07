c5e5a66c86175e2e9662deba82c5dc5296467c86
Janko <janko.huic@gmail.com>
2024-03-07 04:35:22 +0100

    full options-side functionality of categories

--- options.js
+++ options.js
@@ -12,9 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const notifsCheckbox = document.getElementById('notifs');
     const dropdownCheckbox = document.getElementById('extraDropdownCheckbox');
-    
-    let removeNotifs = false;
-    let extraDropdown = false;
 
     notifsCheckbox.addEventListener('change', function() {
         if (notifsCheckbox.checked) {
@@ -40,7 +37,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    function mergeDicts(dict1, dict2) {
+        var mergedDict = {};
+        
+        for (var key in dict1) {
+            if (dict1.hasOwnProperty(key)) {
+                mergedDict[key] = [].concat(dict1[key]);
+            }
+        }
+        
+        for (var key in dict2) {
+            if (dict2.hasOwnProperty(key)) {
+                if (!mergedDict[key]) {
+                    mergedDict[key] = [];
+                }
+                mergedDict[key] = mergedDict[key].concat(dict2[key]);
+            }
+        }
+
+        return mergedDict;
+    }
+
     function submitBlacklist() {
+        if (blacklistInput.value === '') {
+            return;
+        }
         var blacklist = blacklistInput.value.split(',').map(function(item) {return item.trim();});
 
         chrome.storage.sync.get('blacklist', function(storage) {
@@ -68,9 +89,39 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
+    function submitCategories() {
+        chrome.storage.sync.get('categories', function(storage) {
+            input = jsonObject = JSON.parse(categoriesInput.value);
+            console.log(typeof input)
+            if (typeof input === 'object' && Object.keys(input).length > 0){
+                var newCategories = mergeDicts(storage.categories, input);
+            }
+            else return;
+
+            if (storage.categories) {
+                chrome.storage.sync.set({ 'categories': newCategories }, function() {
+                    console.log('categories updated, new categories:');
+                    console.log(newCategories);
+                });
+            } 
+            else {
+                chrome.storage.sync.set({ 'categories': newCategories }, function() {
+                    console.log('categories set');
+                    console.log('categories updated, new categories:');
+                    console.log(newCategories);
+                });
+            }
+            categoriesInput.value = '';
+        });
+    }
+
     blacklistInput = document.getElementById('blacklistInput');
     blacklistSubmit = document.getElementById('blacklistSubmit');
     clearBlacklist = document.getElementById('clearBlacklist');
+    
+    categoriesInput = document.getElementById('categoriesInput');
+    categoriesSubmit = document.getElementById('categoriesSubmit');
+    clearCategories = document.getElementById('clearCategories');
 
     blacklistSubmit.addEventListener('click', submitBlacklist);
     blacklistInput.addEventListener('keydown', function(event) {
@@ -79,10 +130,22 @@ document.addEventListener('DOMContentLoaded', function() {
             submitBlacklist();
         }
     });
+    categoriesSubmit.addEventListener('click', submitCategories);
+    categoriesInput.addEventListener('keydown', function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            submitCategories();
+        }
+    });
 
     clearBlacklist.addEventListener('click', function() {
         chrome.storage.sync.set({ 'blacklist': [] }, function() {
             console.log('Blacklist cleared');
+        });
+    });
+    clearCategories.addEventListener('click', function() {
+        chrome.storage.sync.set({ 'categories': {} }, function() {
+            console.log('categories cleared');
         });
     });
 
@@ -93,6 +156,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert(storage.blacklist.join('\n'));
             } else {
                 alert('Blacklist is empty');
+            }
+        });
+    }); 
+    showCategories.addEventListener('click', function() {
+        chrome.storage.sync.get('categories', function(storage) {
+            if (storage.categories) {
+                console.log(storage.categories);
+                alert(JSON.stringify(storage.categories, null, 2));
+            } else {
+                alert('categories is empty');
             }
         });
     });
