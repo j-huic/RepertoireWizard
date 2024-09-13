de259faa0f1e7bde1848e4a3c022cf28691f44de
Janko <janko.huic@gmail.com>
2024-09-13 21:59:07 +0200

    now adds move cards when there are no games in record; also works with faster scrolling

--- moveHighlighting.js
+++ moveHighlighting.js
@@ -13,6 +13,29 @@ let mainObserver = new MutationObserver(() => {
   }
 });
 
+let mainObserverTwo = new MutationObserver(() => {
+  explorer = document.getElementsByClassName("data")[0];
+
+  if (explorer) {
+    let observer = new MutationObserver(checkFenChange);
+    let config = { attributes: true, subtree: true };
+    observer.observe(explorer, config);
+  }
+});
+
+let mainObserverAlt = new MutationObserver(() => {
+  if (hasFenChanged()) {
+    dataDiv = document.getElementsByClassName("data")[0];
+
+    if (dataDiv && !dataDiv.className.includes("empty")) {
+      let observer = new MutationObserver(moveSelectorDisplay);
+      observer.observe(dataDiv, { childList: true, subtree: true });
+    } else if (dataDiv && dataDiv.className.includes("empty")) {
+      addMoveToEmpty();
+    }
+  }
+});
+
 let observerStatic = new MutationObserver(function (mutations) {
   let wiki = document.getElementsByClassName("analyse__wiki empty")[0];
   if (wiki) {
@@ -21,18 +44,61 @@ let observerStatic = new MutationObserver(function (mutations) {
   }
 });
 
-mainObserver.observe(document, { childList: true, subtree: true });
+mainObserverAlt.observe(document, { childList: true, subtree: true });
 observerStatic.observe(document, { childList: true, subtree: true });
 
 // functions
 
-function moveSelectorDisplay() {
-  let tbody = document.getElementsByTagName("tbody")[0];
-  let fen = tbody.attributes[0].value;
+function debounce(func, wait = 10) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
 
-  let courseMoves = displayMoves(getPureFen(fen), options.sideAgnostic);
-  let allCourseMoves = Object.values(courseMoves).flat();
-  let uniqueCourseMoves = Array.from(new Set(allCourseMoves));
+function addMoveToEmpty() {
+  let empty = document.getElementsByClassName("data empty")[0];
+  let msg = empty.getElementsByClassName("message")[0];
+  if (msg) {
+    empty.removeChild(msg);
+  }
+
+  if (document.getElementsByClassName("moves").length === 0) {
+    var movesDiv = document.createElement("table");
+    let newHeader = document.createElement("thead");
+    movesDiv.className = "moves";
+    newHeader.innerHTML = getHeaderHTML();
+    movesDiv.appendChild(newHeader);
+    j;
+    empty.appendChild(movesDiv);
+  }
+
+  let fen = getFen();
+  moves = moveRecommendationsFromFen(fen);
+
+  if (moves.length === 0) {
+    return;
+  } else {
+    let newTbody = document.createElement("tbody");
+    newTbody.setAttribute("data-fen", fen);
+
+    for (let move of moves) {
+      let tr = document.createElement("tr");
+      tr.innerHTML = getMoveCardHTML(move);
+      newTbody.appendChild(tr);
+    }
+
+    movesDiv.appendChild(newTbody);
+  }
+}
+
+function moveSelectorDisplay(bla = null) {
+  fen = getFen();
+  let tbody = document.getElementsByTagName("tbody")[0];
+  let uniqueCourseMoves = moveRecommendationsFromFen(fen);
 
   for (let move of tbody.children) {
     moveText = move.getElementsByTagName("td")[0].textContent;
@@ -40,6 +106,13 @@ function moveSelectorDisplay() {
       move.getElementsByTagName("td")[0].style.color = "red";
     }
   }
+}
+
+function moveRecommendationsFromFen(fen) {
+  let courseMoves = displayMoves(getPureFen(fen), options.sideAgnostic);
+  let allCourseMoves = Object.values(courseMoves).flat();
+  let uniqueCourseMoves = Array.from(new Set(allCourseMoves));
+  return uniqueCourseMoves;
 }
 
 function getSide() {
@@ -70,13 +143,31 @@ function displayMoves(fen, sideAgnostic = false) {
   return displays;
 }
 
-function getFen() {
+function getFenOld() {
   let tbody = document.querySelector("tbody");
   if (!tbody) {
     return null;
   }
   let fen = tbody.attributes[0].value;
   return fen;
+}
+
+function getFen() {
+  tbody = document.querySelector("tbody");
+  if (tbody) {
+    return tbody.attributes["data-fen"].value;
+  }
+
+  let fenBox = document.querySelector("input.copyable");
+  if (fenBox) {
+    return fenBox.value;
+  } else {
+    let fenBoxAlt = document.querySelector("input.copy-me__target");
+    if (fenBoxAlt) {
+      return fenBoxAlt.value;
+    }
+  }
+  return null;
 }
 
 function onMutation(mutationList) {
@@ -109,6 +200,16 @@ function checkFenChange() {
   }
 }
 
+function hasFenChanged() {
+  let newFen = getFen();
+  if (newFen !== oldFen) {
+    oldFen = newFen;
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function getPureFen(fen) {
   let split = fen.split(" ");
   let newFen = split.slice(0, split.length - 2).join(" ");
@@ -135,4 +236,55 @@ function loadOptions() {
       }
     });
   });
+}
+
+async function onReady(document, callback) {
+  if (callback && typeof callback === "function") {
+    if (
+      document.readyState === "complete" ||
+      document.readyState === "interactive"
+    ) {
+      callback();
+    } else {
+      document.addEventListener("DOMContentLoaded", callback);
+    }
+  }
+}
+
+function getFenBox() {
+  let fenBox = document.querySelector("input.copyable");
+  if (fenBox) {
+    return fenBox;
+  } else {
+    let fenBoxAlt = document.querySelector("input.copy-me__target");
+    if (fenBoxAlt) {
+      return fenBoxAlt;
+    }
+  }
+  return null;
+}
+
+function getMoveCardHTML(move) {
+  return `
+    <td style="color: red;">${move}</td>
+    <td>N/A</td>
+    <td>0</td>
+    <td title="">
+      <div class="bar">
+        <span class="white" style="width: 33.3%">0%</span
+        ><span class="draws" style="width: 33.3%">0%</span
+        ><span class="black" style="width: 33.3%">0%</span>
+      </div>
+    </td>
+  `;
+}
+
+function getHeaderHTML() {
+  return `
+      <tr>
+        <th>Move</th>
+        <th colspan="2">Games</th>
+        <th>White / Draw / Black</th>
+      </tr>
+  `;
 }
